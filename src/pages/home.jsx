6ddaed9ee51ef5830bf0d30c6cbd5ea2444b7b8b
Johsam <johsamfcress@gmail.com>
2025-06-15 14:33:07 +0200

    removed vid for performance

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,10 +3,11 @@ export default function HomePage(){
 
     return(
         <div className="relative h-screen w-screen overflow-hidden grid place-content-center">
-            <video autoPlay loop muted className="absolute w-full h-full object-cover" >
+            {/* <video autoPlay loop muted className="absolute w-full h-full object-cover" >
                 <source src="/assets/ghost-cod.desktop.mp4" type="video/mp4" />
                 Your browser doesn't support HTML5 video.
-            </video>
+            </video> */}
+            <img src="/public/assets/background.jpg" alt="background" className="absolute w-full h-full object-cover" />
 
             <div className='relative flex flex-col text-white justify-center text-center m-1 rounded-2xl backdrop-blur-2xl p-4'>
                 <h1 className="text-5xl font-bold text-amber-400 glow-effect uppercase" >Game Box</h1>
