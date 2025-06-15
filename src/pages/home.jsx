603cf68671b3c910ae843ae2256861baffe5b6a0
Johsam <johsamfcress@gmail.com>
2025-06-15 14:16:23 +0200

    direction

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ export default function HomePage(){
     return(
         <div className="relative h-screen w-screen overflow-hidden grid place-content-center">
             <video autoPlay loop muted className="absolute w-full h-full object-cover" >
-                <source src="/public/assets/ghost-cod.desktop.mp4" type="video/mp4" />
+                <source src="/assets/ghost-cod.desktop.mp4" type="video/mp4" />
                 Your browser doesn't support HTML5 video.
             </video>
 
